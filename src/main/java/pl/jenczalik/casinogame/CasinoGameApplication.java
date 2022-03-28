a3ca51b804645b563d71906c362aa9a1a5cb3c53
wojciech-jenczalik <wjenczalik@netguardians.ch>
2022-03-28 19:12:10 +0200

    Setup spring boot application template

---
+++ src/main/java/pl/jenczalik/casinogame/CasinoGameApplication.java
@@ -0,0 +1,13 @@
+package pl.jenczalik.casinogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinoGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasinoGameApplication.class, args);
+    }
+
+}
